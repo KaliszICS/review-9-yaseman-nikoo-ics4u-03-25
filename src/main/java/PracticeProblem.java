6831ccdd3b42b17ed67f22f954e074f9ef717e2e
yaseman-nikoo-ics4u-03-25 <yaseman.nikoo@student.tdsb.on.ca>
2025-02-19 22:00:37 +0000

    modify

--- src/main/java/PracticeProblem.java
+++ src/main/java/PracticeProblem.java
@@ -3,5 +3,17 @@ public class PracticeProblem {
 	public static void main(String args[]) {
 
 	}
-
+	public static int letterToFive(String sentence, char letter){
+		int numletter=0;
+		int x=0
+		while (x<senetence.length()){
+			if sentence[x].isequal(letter){
+				numletter=+1
+			}
+			x=+1
+		}
+	if (numletter==5){
+		return 
+	}
+	}
 }
