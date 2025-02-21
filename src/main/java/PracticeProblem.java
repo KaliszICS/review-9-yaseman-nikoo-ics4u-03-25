970ea9b0bb2194240400bb15231b08c05711ac19
yaseman-nikoo-ics4u-03-25 <yaseman.nikoo@student.tdsb.on.ca>
2025-02-21 18:50:44 +0000

    modify

--- src/main/java/PracticeProblem.java
+++ src/main/java/PracticeProblem.java
@@ -1,19 +1,44 @@
 public class PracticeProblem {
-
+	
 	public static void main(String args[]) {
+		
+	}
 
-	}
-	public static int letterToFive(String sentence, char letter){
+	public static int lettersToFive(String sentence, char letter){
 		int numletter=0;
-		int x=0
-		while (x<senetence.length()){
-			if sentence[x].isequal(letter){
-				numletter=+1
+		int x=0;
+		while (x<sentence.length()){
+			if (sentence.charAt(x)==letter){
+				numletter++;
 			}
-			x=+1
+			x++;
+			if (numletter==5){
+				return x;
+				}
+			}
+	return -1;
+	}
+
+	public static int countLetter(String sentence, char letter){
+		int num=0;
+		for (int i =0; i< sentence.length();i++){
+			if (sentence.charAt(i)==letter){
+				num++;
+			}
 		}
-	if (numletter==5){
-		return 
+		return num;
 	}
+ public static int oddSum(int num1, int num2){
+	int sum=0;
+	int num= num1;
+	for (int i=0;i<(num2-num1+1); i++){
+		if (num%2!=0){
+			sum=sum+num;
+		}
+		num++;
 	}
+	return sum;
+ }
+
 }
+
